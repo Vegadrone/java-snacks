e4de3c6826cd05000a707f7fcb5a22e7151ecb85
Vegadrone <spenaantonino@gmail.com>
2022-11-16 12:42:28 +0100

    snack 4

--- src/org/generation/italy/JavaSnacks4.java
+++ src/org/generation/italy/JavaSnacks4.java
@@ -1,5 +1,19 @@
 package org.generation.italy;
 
-public class JavaSnacks4 {
+import java.util.Random;
 
+public class JavaSnacks4 {
+	public static void main(String[] args) {
+		
+		Random rnd = new Random();
+		
+		int [] numbers = new int [5];
+		int [] numbers2 = new int [15];
+		
+		int arrayDiff = numbers2.length - numbers.length;
+		
+		for ( int i = 0; i < arrayDiff; i++) {
+			System.out.println(rnd.nextInt(11));
+		}
+	}
 }

dd61e76350baa1681061f536cb420b0ebd78bc09
Vegadrone <spenaantonino@gmail.com>
2022-11-16 16:28:29 +0100

    reverse to snack 7

--- src/org/generation/italy/JavaSnacks7.java
+++ src/org/generation/italy/JavaSnacks7.java
@@ -5,11 +5,17 @@ public class JavaSnacks7 {
 		String[] words = { "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit" };
 		
 		String phrase = "";
+		String reversePhrase = "";
 		
 		for(int i = 0; i < words.length; i++) {
 			phrase += words[i] + " ";			
 		}
 		System.out.println(phrase);
+		
+		 for (int i = words.length - 1; i > -1; i--) {
+			 reversePhrase += words[i] + " ";
+		 }
+		 System.out.println(reversePhrase);
 		
 	}
 }
